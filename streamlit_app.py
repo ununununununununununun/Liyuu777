f28858e53bde18906e9e14e09dbb39ac5e0dcc43
ununununununununununun <144980890+ununununununununununun@users.noreply.github.com>
2023-09-21 04:39:46 +0000

    jjj

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as set
 import random 
 (st.title("おみくじアプリ")
 if st.button("おみくじを引く")
-    results={"大吉","中吉","小吉","吉","凶","大凶"}
+    results=["大吉","中吉","小吉","吉","凶","大凶"]
     result=random.choice(results)
     st.write(f"結果:{result}")
 
